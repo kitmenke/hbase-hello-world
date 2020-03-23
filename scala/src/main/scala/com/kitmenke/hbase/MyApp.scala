d73f4bcecda0a6345c296136247ae0e8d6199fca
Kit Menke <cwmenke@gmail.com>
2020-03-22 20:54:34 -0500

    HBase client written in scala.

---
+++ scala/src/main/scala/com/kitmenke/hbase/MyApp.scala
@@ -0,0 +1,28 @@
+package com.kitmenke.hbase
+
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Get}
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.log4j.Logger
+
+object MyApp {
+  lazy val logger: Logger = Logger.getLogger(this.getClass)
+
+  def main(args: Array[String]): Unit = {
+    logger.info("MyApp starting...")
+    var connection: Connection = null
+    try {
+      val conf = HBaseConfiguration.create()
+      conf.set("hbase.zookeeper.quorum", "hbase-docker")
+      connection = ConnectionFactory.createConnection(conf)
+      val table = connection.getTable(TableName.valueOf("table-name"))
+      val get = new Get(Bytes.toBytes("row-key"))
+      val result = table.get(get)
+      logger.debug(result)
+    } catch {
+      case e: Exception => logger.error("Error in main", e)
+    } finally {
+      if (connection != null) connection.close()
+    }
+  }
+}
---
+++ scala/src/test/scala/com/kitmenke/hbase/MyAppTests.scala
@@ -0,0 +1,11 @@
+package com.kitmenke.hbase
+
+import org.scalatest.FunSpec
+
+class MyAppTests extends FunSpec {
+  describe("my app tests") {
+    it("should be a basic test case") {
+      assert(1 === 1)
+    }
+  }
+}
