1094c7639fc8f2d8238a0430bd2f5d26b112f1dc
Kit Menke <cwmenke@gmail.com>
2020-10-19 15:56:35 -0500

    Remove python project and move Scala project to the top level.

--- python/hbasehelloworld/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__version__ = '0.0.1'
-__author__ = 'Kit Menke'
-
-# Add all modules here to make them importable in testing
-__all__ = ['hbase_client']
-
-from . import *
--- python/tests/test_hbase_client.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import hbasehelloworld
-
-def test_basic():
-    """ basic test which does nothing
-    """
-    assert 1 == 1
